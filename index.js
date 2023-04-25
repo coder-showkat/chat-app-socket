cf8019fd27c01bf1d54b090695deebcac3f4f816
coder-showkat <showkat.coder@gmail.com>
2023-04-25 06:08:56 +0600

    initial commit

---
+++ index.js
@@ -0,0 +1,21 @@
+const io = require("socket.io")(8800, {
+    cors: {
+        origin: "https://chat-app-sam.netlify.app"
+    }
+})
+
+io.on("connection", (socket)=>{
+
+    console.log("A user has connected!");
+
+    // listen for incoming message from the client side
+    socket.on("message", msg=>{
+        io.emit("message", msg);
+    });
+
+
+    // disconnect socket when the user disconnect
+    socket.on("disconnect", ()=>{
+        console.log("A user has disconnected!")
+    })
+})
